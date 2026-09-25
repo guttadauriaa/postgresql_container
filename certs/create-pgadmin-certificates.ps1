07279d7341f214910e4bd54343667d0f1f41490c
Adriano Guttadauria <adriano.guttadauria@umons.ac.be>
2026-09-25 17:30:12 +0200

    Add cert to pgadmin and trying TLS options.

---
+++ certs/create-pgadmin-certificates.ps1
@@ -0,0 +1,13 @@
+Write-Host "Requires certstrap for the creation of auto-signed certificates"
+Write-Host "\$ git clone https://github.com/square/certstrap"
+Write-Host "\$ cd certstrap"
+Write-Host "\$ go build"
+Write-Host "`nThese commands will create a binary called certstrap under the project root directory."
+Write-Host "`nRequires Go version 1.18+"
+
+Write-Host "Requesting a certificate and its keypair."
+Write-Host "Don't set a password for the certificate request"
+certstrap request-cert --common-name localhost --domain localhost --domain pgadmin
+
+Write-Host "Signing the request and generating the certificate."
+certstrap sign localhost --CA myCA
